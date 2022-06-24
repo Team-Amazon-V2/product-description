825c6896d35f94d2dc413a0134f1f98d03a4fc38
magdielrosario97 <magdielrosario97@gmail.com>
2022-06-24 17:13:23 -0500

    created fetch request successfully and fixed cors issue

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from "react";
 
+class App extends React.Component {
+   state = {
+      loading: true,
+      product: null,
+      loadingMsg: "loading product...",
+   };
+
+   componentDidMount() {
+      fetch("http://localhost:3005/")
+         .then((res) => res.json())
+         .then((data) => {
+            console.log(data);
+            this.setState({ loading: false, product: data });
+         });
+   }
+
+   render() {
+      if (this.state.loading) {
+         return <h1>{this.state.loadingMsg}</h1>;
+      }
+      return <div>This worked</div>;
+   }
+}
+
+export default App;
