dd71e07d9cd28af33c2d98b68701822656532278
magdielrosario97 <magdielrosario97@gmail.com>
2022-06-29 15:20:53 -0500

    added asin column to db

--- database/db/migration.sql
+++ database/db/migration.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS product;
 
 CREATE TABLE product(
     id SERIAL UNIQUE PRIMARY KEY,
+    asin_id VARCHAR,
     product_name TEXT,
     category TEXT,
     price NUMERIC,
@@ -13,6 +14,6 @@ CREATE TABLE product(
     about TEXT ARRAY
 );
 
-INSERT INTO product (product_name, category, price, options, about)
-    VALUES ('2022 Apple iPad Air (10.9-inch, Wi-Fi, 64GB) - Space Gray (5th Generation)', 'electronics', 569.00, ARRAY['WiFi', 'Space Gray', '64GB'],
+INSERT INTO product (asin_id, product_name, category, price, options, about)
+    VALUES ('B09V3HN1KC', '2022 Apple iPad Air (10.9-inch, Wi-Fi, 64GB) - Space Gray (5th Generation)', 'electronics', 569.00, ARRAY['WiFi', 'Space Gray', '64GB'],
     ARRAY ['10.9-inch Liquid Retina display with True Tone, P3 wide color, and an antireflective coating', 'Apple M1 chip with Neural Engine', '12MP Wide camera', '12MP Ultra Wide front camera with Center Stage', 'Up to 256GB of storage', '5G capable']);
